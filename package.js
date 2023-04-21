8526011ca4cfac98755667e78be4ed9b1d61b5a9
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:45:07 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
     name: 'xerdi:common-helpers',
-    version: '0.0.2',
+    version: '0.0.3',
     summary: 'Common Meteor Template helpers',
     git: 'https://github.com/Xerdi/meteor-common-helpers.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.8.0');
+    api.versionsFrom('2.11.0');
     api.use([
         'ecmascript',
-        'blaze-html-templates@1.2.1'
+        'blaze-html-templates@2.0.0'
     ]);
     api.mainModule('helpers.js', 'client');
 });
